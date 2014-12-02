f9ad41864d1acaef01ee5877fd3fb40fa1d1e169
Pablo--Martinez <p.martinezben@gmail.com>
2014-12-01 22:45:52 -0200

    Ahora envia el estado en tiempo real de cada sensor de puerta al servidor y se visualiza, ademas se hace la prueba de varios sensores conectados a la vez

--- sensorPuerta.py
+++ sensorPuerta.py
@@ -3,6 +3,7 @@
 import time
 import RPi.GPIO as GPIO
 import threading
+import urllib2
 import alertas
 
 #eligiendo el modo de numeracion de los GPIO
@@ -18,11 +19,15 @@ GPIO.setup(LED_PUERTA,GPIO.OUT)
 #Path donde se encuentra el archivo de configuracion
 PATH_CONF = "/home/pi/Monitor/rpi.conf"
 
+#Constantes del emoncms
+HOST_EMONCMS = "10.8.0.1"
+TIME_OUT = 5
+
 def alertar(alerta):
 	alerta['ALERTANDO'] = True
 	alertas.activarAlertaSonora(alerta)
 
-def interrupcionPuerta(pin,tiempo_abierto,alerta):
+def interrupcionPuerta(dispositivo,apikey,pin,tiempo_abierto,alerta):
 	empezo = False
 
 	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -38,6 +43,8 @@ def interrupcionPuerta(pin,tiempo_abierto,alerta):
 			timer = threading.Timer(tiempo_abierto,alertar,args=(alerta,))
 			timer.start()
 			GPIO.output(LED_PUERTA,1)
+			url = "http://%s/bioguard/input/post.json?json={%s:%i}&apikey=%s" % (HOST_EMONCMS,dispositivo+"_Contacto"+str(PINES.index(pin)+1),1,apikey)
+                        urllib2.urlopen(url,timeout=TIME_OUT)
 		
 		else:
 			GPIO.wait_for_edge(pin, GPIO.FALLING)
@@ -46,8 +53,10 @@ def interrupcionPuerta(pin,tiempo_abierto,alerta):
 			print "Tiempo abierto: %f \n" % time_end
 			timer.cancel()
 			GPIO.output(LED_PUERTA,0)
+			url = "http://%s/bioguard/input/post.json?json={%s:%i}&apikey=%s" % (HOST_EMONCMS,dispositivo+"_Contacto"+str(PINES.index(pin)+1),0,apikey)
+			urllib2.urlopen(url,timeout=TIME_OUT)
 
-def sensorPuerta(apikey,digitales,tiempo_apertura,alerta):
+def sensorPuerta(dispositivo,apikey,digitales,tiempo_apertura,alerta):
 	#Configuro el pin de la alerta visual de la puerta y lo seteo apagado
 	GPIO.setup(LED_PUERTA,GPIO.OUT)
 	GPIO.output(LED_PUERTA,0)
@@ -55,7 +64,7 @@ def sensorPuerta(apikey,digitales,tiempo_apertura,alerta):
 	for i in range(6):
 		if(digitales[i] != "NULL"):
 			if(digitales[i] == "Contacto"):
-				t = threading.Thread(target=interrupcionPuerta,args=(PINES[i],tiempo_apertura,alerta))
+				t = threading.Thread(target=interrupcionPuerta,args=(dispositivo,apikey,PINES[i],tiempo_apertura,alerta))
 				t.start()
 			else:
 				#TODO
